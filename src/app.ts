7eacb77ff1d8f14bc97b62c98cb4f8905669cf15
WilliamOnyejiaka <williamonyejiaka08062528003@gmail.com>
2024-05-22 10:25:02 +0100

    built

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,18 @@ import { redisConfig,corsConfig } from "./config";
 import Redis from 'ioredis';
 import { auth } from "./routes";
 
-// import { Model,Users } from "./models";
 
 function createApp() {
     const app: Application = express();
     const redisEnv = redisConfig('dev');
-    const redisClient = new Redis(redisEnv!);
+    // const redisClient = new Redis(redisEnv!);
 
     app.use(express.urlencoded({ extended: true }));
     app.use(corsConfig);
 
     app.use(express.json());
     app.use(morgan("combined"));
-    app.use((req: Request, res: Response, next: NextFunction) => { res.locals['redisClient'] = redisClient;next() });
+    // app.use((req: Request, res: Response, next: NextFunction) => { res.locals['redisClient'] = redisClient;next() });
 
     app.use("/api/auth",auth);
 
@@ -33,17 +32,17 @@ function createApp() {
 
     });
 
-    app.get("/test1", async (req: Request, res: Response) => {
-        try {
-            const value = await redisClient.get("user");
-            return res.status(200).json({
-                'error': false,
-                'message': value
-            });
-        } catch (err) {
-            console.error('Error creating item:', err);
-        }
-    });
+    // app.get("/test1", async (req: Request, res: Response) => {
+    //     try {
+    //         const value = await redisClient.get("user");
+    //         return res.status(200).json({
+    //             'error': false,
+    //             'message': value
+    //         });
+    //     } catch (err) {
+    //         console.error('Error creating item:', err);
+    //     }
+    // });
 
     app.get("/", (req: Request, res: Response) => {
         res.status(200).json({
