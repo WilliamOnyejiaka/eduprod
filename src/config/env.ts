fbcfa164df97f9e94f338743f46eb53ae16dc7c4
WilliamOnyejiaka <williamonyejiaka08062528003@gmail.com>
2024-05-22 10:08:35 +0100

    first commit

---
+++ src/config/env.ts
@@ -0,0 +1,15 @@
+import { config } from "dotenv";
+
+config();
+
+export default function env(key: string){
+    return {
+        'port': process.env.PORT!,
+        'secretKey': process.env.SECRET_KEY!,
+        'envType': process.env.ENV_TYPE ?? "prod",
+        'databaseURL': process.env.DATABASE_URL!,
+        'accessTokenSecret': process.env.ACCESS_TOKEN_SECRET!,
+        'redisURL': process.env.REDIS_URL!
+    }[key];
+}
+
